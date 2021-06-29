0cd6644ba4c8ae46a49cac8fef4785972ab3423e
Aaron Dereszynski <aarondski123@gmail.com>
2021-06-29 11:49:44 -0600

    commit tree c7cb8be8f5250865e4f9b64d4a59779631d8b80f

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNum) {
+  let free
+  if (someNum <= 400) {
+    free = 'This one is on me!'
+    return free
+  }
+  let dock
+  if (someNum >= 2000 && someNum <= 2500) {
+    dock = 'I will gladly take your thirty bucks.'
+    return dock
+  }
+  else if (someNum >= 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const isCity = (city === "NYC") ? 'Ok, sounds good.' :'No go.'
+  return isCity
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  const fareTip = tip
+  switch (fareTip){
+    case 'generous' : return ("Thank you so much.")
+    break;
+    case 'not as generous' : return ('Thank you.')
+    break;
+    case 'thanks for everything' : return ('Bye.')
+    break;
+    
+
+  }
+    
+  
 }
